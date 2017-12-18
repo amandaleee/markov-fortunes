18dfa544cc83fbe7a5bfe0c03b83adfc1d800b0f
amanda lee anderson <amandalee.anderson@gmail.com>
2017-12-18 10:11:28 -0600

    gitignore + some style stuff

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 
 require 'markov.php';
 
+function markov() {
+}
+
 if (isset($_POST['submit'])) {
+    
     $order  = 5;
-    $length = 200;
+    $length = 140;
 
     $text = file_get_contents("text/fortunes.txt");
 
@@ -16,7 +20,7 @@ if (isset($_POST['submit'])) {
     // end with punctuation;
     //begin and end with a real word. 
     //add periods to the end of fortunes
-    //
+    //do this on page load and then again when user hovers - ajax request
 
 }
 ?>
@@ -24,19 +28,32 @@ if (isset($_POST['submit'])) {
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
-    <title>Artist Statement Generator</title>
+    <title>Fortune</title>
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />    
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
-    <h1>Tap for fortune</h1>
-    <form method="post" action="" name="markov">
-        <input type="submit" name="submit" value="GO" />
-    </form>
+    
 
-    <?php if (isset($markov)) : ?>
-        <p style="max-width: 350px;"><?php echo $markov; ?></p>
-    <?php endif; ?>
+    
+
+    <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+    <div class="flipper">
+        <div class="front">
+            <h1>Tap for fortune</h1>
+            <form method="post" action="" name="markov">
+                <input type="submit" name="submit" value="GO" />
+            </form>
+        </div>
+        <?php if (isset($markov)) : ?>
+            <!-- back content -->
+            <div class="back">
+                <p style="max-width: 350px;"><?php echo $markov; ?></p>
+            </div>
+        <?php endif; ?>
+    </div>
+</div>
 
   
 </body>
