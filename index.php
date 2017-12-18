606b113e31363ba242ed050222e69965d63325b2
amanda lee anderson <amandalee.anderson@gmail.com>
2017-12-18 11:05:06 -0600

    moving the markov function out of the index file; adding jquery

--- index.php
+++ index.php
@@ -1,39 +1,3 @@
-<?php
-
-require 'markov.php';
-
-if (isset($_POST['submit'])) {
-    $order  = 5;
-    $length = 120; //this will get shorter after we manipulate it below
-
-    $text = file_get_contents("text/fortunes.txt");
-
-    if(isset($text)) {
-        $markov_table = generate_markov_table($text, $order);
-        $markov = generate_markov_text($length, $markov_table, $order);
-    }
-
-    //explode at the first space so we can be sure we start with a full word. 
-    $markov_result_1 = explode(" ", $markov, 2); 
-
-    //explode at the last space so we can be sure we end with a full word. 
-    // reverse the string
-    $markov_result_1_rev = strrev($markov_result_1[1]); 
-    //explode at the first space [actually the last bc it's reveresed]
-    $markov_result_2_rev = explode(" ", $markov_result_1_rev, 2);
-    //reverse it again to set it right
-    //also upcase the first character here
-    $markov_result_2 = ucfirst(strrev($markov_result_2_rev[1])); 
-   
-     //add a period to the end of the fortune, if it's not already there. 
-    if (strrpos($markov_result_2 , ".") !== 140) {
-        $markov_result = $markov_result_2 . ".";
-    } else {
-        $markov_result = $markov_result_2; 
-    }
-
-}
-?>
 <!doctype html>
 <html>
 <head>
@@ -42,6 +6,12 @@ if (isset($_POST['submit'])) {
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />    
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="style.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            // alert("yup");
+        });
+    </script>
 </head>
 <body>
     
